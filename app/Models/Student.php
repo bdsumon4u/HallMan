2b579be6eebe121d1b49d7b9012458d4a051ec55
Sumon Ahmed <bdsumon4u@gmail.com>
2024-07-07 20:50:39 +0600

    Fee Resource

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Student extends Model
 {
@@ -13,5 +14,10 @@ class Student extends Model
     public function hall(): BelongsTo
     {
         return $this->belongsTo(Hall::class);
+    }
+
+    public function fees(): HasMany
+    {
+        return $this->hasMany(Fee::class);
     }
 }
